d6fc4355a871bfb3765606126885bcbd65fe0033
William Phelps <williampaul@phelpsfamily.org>
2024-07-18 13:53:46 -0600

    chore: Update svelte.config.js to include 'node_modules/flowbite-svelte'

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			routes: {
-				include: ['/*'],
+				include: ['/*', 'node_modules/flowbite-svelte'],
 				exclude: ['<all>'],
 			},
 		}),
