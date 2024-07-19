28dc1fb46a959315a474f983e093626d7ea283d3
William Phelps <williampaul@phelpsfamily.org>
2024-07-18 19:57:25 -0600

    Add AUTH_RESEND_KEY to auth

--- src/lib/auth/auth.js
+++ src/lib/auth/auth.js
@@ -4,12 +4,13 @@ import clientPromise from '$lib/server/db.js';
 import Resend from '@auth/sveltekit/providers/resend';
 import Google from '@auth/sveltekit/providers/google';
 
-import { AUTH_SECRET } from '$env/static/private';
+import { AUTH_RESEND_KEY, AUTH_SECRET } from '$env/static/private';
 
 export const { handle, signIn } = SvelteKitAuth({
 	adapter: MongoDBAdapter(clientPromise),
 	providers: [
 		Resend({
+			apiKey: AUTH_RESEND_KEY,
 			from: 'william.phelps@canyonedgetech.com',
 		}),
 		Google,
