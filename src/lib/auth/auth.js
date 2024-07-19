8e0cdad1f17a745b6dbd75a63bf629a1629b034e
William Phelps <williampaul@phelpsfamily.org>
2024-07-18 19:32:12 -0600

    Add AUTH_SECRET to config

--- src/lib/auth/auth.js
+++ src/lib/auth/auth.js
@@ -1,15 +1,18 @@
-import { SvelteKitAuth } from "@auth/sveltekit";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "$lib/server/db.js";
-import Resend from "@auth/sveltekit/providers/resend";
-import Google from "@auth/sveltekit/providers/google";
- 
+import { SvelteKitAuth } from '@auth/sveltekit';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import clientPromise from '$lib/server/db.js';
+import Resend from '@auth/sveltekit/providers/resend';
+import Google from '@auth/sveltekit/providers/google';
+
+import { AUTH_SECRET } from '$env/static/private';
+
 export const { handle, signIn } = SvelteKitAuth({
-    adapter: MongoDBAdapter(clientPromise),
-    providers: [
-        Resend({
-            from: "william.phelps@canyonedgetech.com"
-        }),
-        Google
-    ]
+	adapter: MongoDBAdapter(clientPromise),
+	providers: [
+		Resend({
+			from: 'william.phelps@canyonedgetech.com',
+		}),
+		Google,
+	],
+	secret: AUTH_SECRET,
 });
