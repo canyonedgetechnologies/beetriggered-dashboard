16f67f1d04240037539a81e5660ae11177c4931a
William Phelps <williampaul@phelpsfamily.org>
2024-07-18 20:30:42 -0600

    Add Google AUTH Keys

--- src/lib/auth/auth.js
+++ src/lib/auth/auth.js
@@ -4,7 +4,12 @@ import clientPromise from '$lib/server/db.js';
 import Resend from '@auth/sveltekit/providers/resend';
 import Google from '@auth/sveltekit/providers/google';
 
-import { AUTH_RESEND_KEY, AUTH_SECRET } from '$env/static/private';
+import {
+	AUTH_GOOGLE_ID,
+	AUTH_GOOGLE_SECRET,
+	AUTH_RESEND_KEY,
+	AUTH_SECRET,
+} from '$env/static/private';
 
 export const { handle, signIn } = SvelteKitAuth({
 	adapter: MongoDBAdapter(clientPromise),
@@ -13,7 +18,10 @@ export const { handle, signIn } = SvelteKitAuth({
 			apiKey: AUTH_RESEND_KEY,
 			from: 'william.phelps@canyonedgetech.com',
 		}),
-		Google,
+		Google({
+			clientId: AUTH_GOOGLE_ID,
+			clientSecret: AUTH_GOOGLE_SECRET,
+		}),
 	],
 	secret: AUTH_SECRET,
 });
